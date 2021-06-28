976d1ab98d7c16c6e6e9f2a46332db03cd15e6db
TheRealDan <thedanielhailwood@gmail.com>
2021-06-28 18:51:24 +1200

    create new empty libgdx project

---
+++ desktop/src/dev/therealdan/tetris/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dev.therealdan.tetris.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.therealdan.tetris.Tetris;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Tetris(), config);
+	}
+}
