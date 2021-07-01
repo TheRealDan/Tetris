404174fdb809a1610017bd03afc29cef0d04e797
TheRealDan <thedanielhailwood@gmail.com>
2021-07-01 13:49:56 +1200

    Square: render same as Tetrimino

--- core/src/dev/therealdan/tetris/game/Square.java
+++ core/src/dev/therealdan/tetris/game/Square.java
@@ -16,8 +16,13 @@ public class Square {
     }
 
     public void render(ShapeRenderer shapeRenderer, PlayField playField) {
+        float size = playField.getCellSize();
+        shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(getColor());
-        shapeRenderer.rect(playField.getX(getX()), playField.getY(getY()), playField.getCellSize(), playField.getCellSize());
+        shapeRenderer.rect(playField.getX(getX()), playField.getY(getY()), size, size);
+        shapeRenderer.set(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.BLACK);
+        shapeRenderer.rect(playField.getX(getX()), playField.getY(getY()), size, size);
     }
 
     public Color getColor() {
