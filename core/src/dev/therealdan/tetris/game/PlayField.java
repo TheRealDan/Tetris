c2df776695db6831da59603613d2f81e66984870
TheRealDan <thedanielhailwood@gmail.com>
2021-07-01 21:57:21 +1200

    PlayField: better positioning for playfield startX for better screen size responsiveness

--- core/src/dev/therealdan/tetris/game/PlayField.java
+++ core/src/dev/therealdan/tetris/game/PlayField.java
@@ -8,6 +8,7 @@ public class PlayField {
     private int cellsWide;
     private int cellsHigh;
 
+    private float startX;
     private float startY;
     private float cellSize;
 
@@ -19,18 +20,19 @@ public class PlayField {
     public void render(ShapeRenderer shapeRenderer) {
         for (int x = 0; x < cellsWide; x++) {
             for (int y = 0; y < cellsHigh; y++) {
-                shapeRenderer.rect(Gdx.graphics.getWidth() / 3f + x * getCellSize(), startY + y * getCellSize(), getCellSize(), getCellSize());
+                shapeRenderer.rect(startX + x * getCellSize(), startY + y * getCellSize(), getCellSize(), getCellSize());
             }
         }
     }
 
     public void resize(float width, float height) {
         cellSize = Math.min(height / cellsHigh, width / 3f / cellsWide);
+        startX = width / 2f - cellsWide * cellSize / 2f;
         startY = (height - getCellSize() * cellsHigh) / 2f;
     }
 
     public float getX(int cellX) {
-        return Gdx.graphics.getWidth() / 3f + cellX * getCellSize();
+        return startX + cellX * getCellSize();
     }
 
     public float getY(int cellY) {
